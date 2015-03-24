b0466f8534d8c06f6a64c00a631f34dd666e7cf2
Colin Rymer <colin.rymer@gmail.com>
2015-03-24 13:36:13 -0400

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create_with(password: ENV['SUPER_ADMIN_PASSWORD'], password_confirmation: ENV['SUPER_ADMIN_PASSWORD']).find_or_create_by(email: 'colin.rymer@gmail.com')
+
+User.create_with(password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'], first_name: "Colin", last_name: "Rymer", address: "5887 Sunset Canyon Dr.", admin: true, approved: true).find_or_create_by(email: 'colin.rymer@gmail.com')
