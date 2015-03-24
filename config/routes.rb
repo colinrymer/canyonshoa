0e65e5eb45a2f4099d9d610792fb15bf12d2bab9
Colin Rymer <colin.rymer@gmail.com>
2015-03-24 11:15:02 -0400

    fix user routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
 
   devise_for :admins, controllers: { registrations: "admin_registrations" }
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'signin' => 'devise/sessions#new', :as => :new_user_session
+    post 'signin' => 'devise/sessions#create', :as => :user_session
+    delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   mount RailsAdmin::Engine => '/canyons-admin', as: 'rails_admin'
   mount Forem::Engine, at: '/forums'
