07463d48cf6805c26c3c7daf31406b51d742f3c4
Colin Rymer <colin.rymer@gmail.com>
2015-05-18 23:18:01 -0400

    prep for docker deployment

--- Gemfile
+++ Gemfile
@@ -1,45 +1,42 @@
 source 'https://rubygems.org'
 
-ruby '2.2.1'
+gem 'rails', '4.1.10'
 
-gem 'rails', '4.2.0'
+gem 'mysql2'
 
 gem 'comfortable_mexican_sofa'
-gem 'forem', github: 'colinrymer/forem', branch: 'patch-1'
+gem 'forem', github: 'radar/forem', branch: 'rails4'
 
 gem 'puma'
 
 gem 'bitters'
 gem 'bourbon'
-gem 'coffee-rails', '~> 4.1.0'
+gem 'coffee-rails'
 gem 'comfy_blog'
 gem 'devise'
 gem 'execjs'
 gem 'high_voltage'
-gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
 gem 'lograge'
 gem 'neat'
 gem 'normalize-rails'
 gem 'rails_admin'
 gem 'refills'
-gem 'sass-rails', '~> 5.0'
+gem 'sass-rails'
 gem 'simple_form'
 gem 'slim-rails'
 gem 'turbolinks'
-gem 'uglifier', '>= 1.3.0'
+gem 'uglifier'
 gem 'will_paginate'
 
 group :production do
   gem 'rails_12factor'
-  gem 'pg'
 end
 
 group :development, :test do
   gem 'quiet_assets'
   gem 'byebug'
   gem 'spring'
-  gem 'sqlite3'
   gem 'web-console', '~> 2.0'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Admin.create_with(password: ENV['SUPER_ADMIN_PASSWORD'], password_confirmation: ENV['SUPER_ADMIN_PASSWORD']).find_or_create_by(email: ENV['SUPER_ADMIN_EMAIL'])
 
-Admin.create_with(password: ENV['SUPER_ADMIN_PASSWORD'], password_confirmation: ENV['SUPER_ADMIN_PASSWORD']).find_or_create_by(email: 'colin.rymer@gmail.com')
-
-User.create_with(password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'], first_name: "Colin", last_name: "Rymer", address: "5887 Sunset Canyon Dr.", forem_admin: true, admin: true, approved: true).find_or_create_by(email: 'colin.rymer@gmail.com')
+User.create_with(password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'], first_name: ENV['ADMIN_FIRST_NAME'], last_name: ENV['ADMIN_LAST_NAME'], address: ENV['ADMIN_ADDRESS'], forem_admin: true, admin: true, approved: true).find_or_create_by(email: ENV['ADMIN_EMAIL'])
